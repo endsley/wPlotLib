4d974ea91309b5491eda1cd324bfab51ef77d7bf
endsley <chieh.t.wu@gmail.com>
2021-05-15 11:30:51 -0400

    added read me

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
   author = 'Chieh Wu',                   # Type in your name
   author_email = 'chieh.t.wu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/endsley/wPlotLib.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/endsley/wPlotLib/archive/refs/tags/0.01.tar.gz',    # I explain this later on
   keywords = ['plot', 'data'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'matplotlib',
+          'numpy',
       ],
   classifiers=[
-    'Development Status :: 1 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
