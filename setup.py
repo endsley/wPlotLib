105c2007b145fceb97323603f792b884594d75df
endsley <chieh.t.wu@gmail.com>
2021-05-25 12:58:44 -0400

    updating to version 0.03

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from distutils.core import setup
+
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f: 
+	long_description = f.read()
+
 setup(
   name = 'wplotlib',         # How you named your package folder (MyLib)
   packages = ['wplotlib'],   # Chose the same as "name"
-  version = '0.02',      # Start with a small number and increase it with every change you make
+  version = '0.03',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A library that simplifies some basic plotting.',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type='text/markdown',  
   author = 'Chieh Wu',                   # Type in your name
   author_email = 'chieh.t.wu@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/endsley/wPlotLib.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/endsley/wPlotLib/archive/refs/tags/0.02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/endsley/wPlotLib/archive/refs/tags/0.03.tar.gz',    # I explain this later on
   keywords = ['plot', 'data'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
--- wplotlib/lines.py
+++ wplotlib/lines.py
@@ -96,5 +96,5 @@ if __name__ == "__main__":
 	x = np.linspace(0, 10, 1000)
 	y = np.sin(x)
 
-	lp = line_plot()
+	lp = lines()
 	lp.plot_line(x, y, 'Title Here', 'X axis', 'Y axis', imgText=textstr)#, outpath)
