a1d7c0f84358ea6635acbebf81e8658795806372
endsley <chieh.t.wu@gmail.com>
2021-07-27 18:22:24 -0400

    kk

--- syncPypi.py
+++ syncPypi.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import time
 
 os.system('rm -r dist/')
 os.system('python setup.py sdist')
 os.system('twine upload dist/*')
+time.sleep(3)
 os.system('pip install wplotlib -U')
 
