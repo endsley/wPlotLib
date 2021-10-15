7947cf25641ef85f0c8ac8e07ecbac88d975c4e9
endsley <chieh.t.wu@gmail.com>
2021-10-15 10:20:32 -0400

    not important enough to comment

--- examples/code/draw_lines_scatter.py
+++ examples/code/draw_lines_scatter.py
@@ -14,8 +14,17 @@ x = np.linspace(0, 10, 40)
 y = np.sin(x)
 
 lp = wplotlib.lines(figsize=(10,5))		# (width, height)
-lp.plot_line(x, y, 'Title Here', 'X axis', 'Y axis', imgText=textstr, ticker_fontsize=12, ticker_rotate=90)#, outpath)
+lp.plot_line(x, y, 'Title Here', 'X axis', 'Y axis', imgText=textstr, ticker_fontsize=12, xticker_rotate=90)#, outpath)
 
 lp = wplotlib.scatter(figsize=(10,5))		# (width, height)
 lp.plot_scatter(x, y, 'Title Here', 'X axis', 'Y axis', imgText=textstr, ticker_fontsize=8 )#, outpath)
+
+
+lp = wplotlib.lines(figsize=(10,5))		# (width, height)
+lp.plot_line(x, y, 'Title Here', 'X axis', 'Y axis', ticker_fontsize=12, xticker_rotate=90, xtick_locations=[2,4,6], xtick_labels=['A','B','C'])
+
+lp = wplotlib.scatter(figsize=(10,5))		# (width, height)
+lp.plot_scatter(x, y, 'Title Here', 'X axis', 'Y axis', ticker_fontsize=8 , xticker_rotate=90, 
+					ytick_locations=[2,4,6], ytick_labels=['A','B','C'],
+					xtick_locations=[2,4,6], xtick_labels=['A','B','C'])
 
