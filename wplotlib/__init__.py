512e8a219ad8e2f2b35cf0038ded7ab5a29e6b69
endsley <chieh.t.wu@gmail.com>
2021-10-26 16:12:39 -0400

    not important enough to comment

--- wplotlib/__init__.py
+++ wplotlib/__init__.py
@@ -2,6 +2,6 @@ from .lines import lines
 from .scatter import scatter
 from .histograms import histograms
 from .heatMap import heatMap
-from .clusters import cluster_plot
+from .clusters import plot_clusters
 from .bar import bar
 
--- wplotlib/clusters.py
+++ wplotlib/clusters.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import wuml
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.metrics
@@ -9,39 +10,51 @@ from sklearn.preprocessing import normalize			# version : 0.17
 import matplotlib.pyplot as plt
 
 
+class plot_clusters:
+	def __init__(self, X, allocation, title='', xlabel='', ylabel='', 
+					figsize=None, fsize=14, useGrid=True, subplot=None):
+		X = wuml.ensure_numpy(X)
 
+		cmap = ['b', 'g', 'r', 'c', 'm', 'y','k']
+		labels = np.unique(allocation)
+		n = labels.shape[0]
+	
+		if figsize is not None: plt.figure(figsize=figsize) # figsize=(3,2)
+		#f, axs = plt.subplots(2,2,figsize=(15,15))
 
-def cluster_plot(X, allocation, title='', xlabel='X', ylabel='y', fsize=14, useGrid=True):
-	cmap = ['b', 'g', 'r', 'c', 'm', 'y','k']
+		plt.style.use('default')
+		#plt.style.use('seaborn')
+		#plt.style.use("ggplot")
+		#plt.rcParams['axes.edgecolor'] = "#777777"
+		#plt.rcParams['axes.facecolor'] = '#FFFFFF'
+	
+		#ax = plt.axes()
+		#plt.axis([xmin, xmax, ymin, ymax])
+		#ax.set_aspect('equal')
+		#ax.grid(True, which='both')
+	
+		#ax.set(facecolor = 'white')
+		#ax.axhline(y=0, color='k')
+		#ax.axvline(x=0, color='k')
+	
+		if subplot is not None: plt.subplot(subplot)
+		for i, j in enumerate(labels):
+			subX = X[allocation == labels[i]]
+			plt.plot(subX[:,0], subX[:,1], cmap[i] + '.')
+	
+		plt.xlabel(xlabel)
+		plt.ylabel(ylabel)
+		plt.title(title, fontsize=fsize)
+		if useGrid: plt.grid(linestyle='dotted')
+		if subplot is None: 
+			plt.tight_layout()
+			plt.show()
 
-	labels = np.unique(allocation)
-	n = labels.shape[0]
-
-	plt.style.use('default')
-	#plt.style.use('seaborn')
-	#plt.style.use("ggplot")
-	#plt.rcParams['axes.edgecolor'] = "#777777"
-	#plt.rcParams['axes.facecolor'] = '#FFFFFF'
-
-	#ax = plt.axes()
-	#plt.axis([xmin, xmax, ymin, ymax])
-	#ax.set_aspect('equal')
-	#ax.grid(True, which='both')
-
-	#ax.set(facecolor = 'white')
-	#ax.axhline(y=0, color='k')
-	#ax.axvline(x=0, color='k')
-
-	for i, j in enumerate(labels):
-		subX = X[allocation == labels[i]]
-		plt.plot(subX[:,0], subX[:,1], cmap[i] + '.')
-
-	plt.xlabel(xlabel)
-	plt.ylabel(ylabel)
-	plt.title(title, fontsize=fsize)
-	plt.tight_layout()
-	if useGrid: plt.grid(linestyle='dotted')
-	plt.show()
+	def show(self):
+		#plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+		#plt.subplots_adjust(hspace=1.0, wspace=1.0)
+		plt.tight_layout()
+		plt.show()
 
 if __name__ == "__main__":
 	n = 100
